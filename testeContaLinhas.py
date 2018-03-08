3407e2be8b8c25d9f46596aa6ea139a8eaec0bea
claytonchagas <claytonchagas1@gmail.com>
2018-03-08 01:56:34 -0300

    ok

--- testeContaLinhas.py
+++
@@ -1,4 +0,0 @@
-for x in range(0, 1):
-    for y in range(1, 11):
-        print('%d * %d = %d' % (x, y, x*y))
-        
--- testeescrita.py
+++
@@ -1,9 +0,0 @@
-arq = open('lista.txt', 'w')
-texto = []
-texto.append('Lista de Alunos\n')
-texto.append('---\n')
-texto.append('Joao da Silva\n')
-texto.append('Jose Lima\n')
-texto.append('Maria das Dores')
-arq.writelines(texto)
-arq.close()
